52a81aa2d91f0c0f75b5be1d5d4050825f5f4289
Steve Korshakov <steve@korshakov.com>
2020-06-03 14:25:50 -0700

    feat: Ability to customize retry behaviour

--- packages/spacex/src/RetryEngine.ts
+++ packages/spacex/src/RetryEngine.ts
@@ -2,6 +2,10 @@ import { GraphqlEngine, GraphqlEngineStatus, OperationParameters, GraphqlQueryWa
 import { Watcher } from './utils/Watcher';
 import { backoff } from './utils/backoff';
 import { randomKey } from './utils/randomKey';
+import { delay } from './utils/delay';
+import { exponentialBackoffDelay } from './utils/backoff';
+
+export type RetryErrorHandler = (src: Error) => { type: 'error', error: Error } | { type: 'unknown' } | { type: 'retry', delay: number };
 
 export class RetryEngine implements GraphqlEngine {
 
@@ -15,13 +19,16 @@ export class RetryEngine implements GraphqlEngine {
     }
 
     private inner: GraphqlEngine;
-    private errorHandler: (src: Error) => Error | null;
+    private errorHandler: RetryErrorHandler;
+    private minRetry: number;
     constructor(opts: {
         engine: GraphqlEngine,
-        errorHandler: (src: Error) => Error | null
+        errorHandler: RetryErrorHandler,
+        minRetry: number
     }) {
         this.errorHandler = opts.errorHandler;
         this.inner = opts.engine;
+        this.minRetry = opts.minRetry;
         this.statusWatcher.setState(this.inner.status);
         this.inner.watchStatus((s) => {
             this.statusWatcher.setState(s);
@@ -30,17 +37,20 @@ export class RetryEngine implements GraphqlEngine {
 
     async query<TQuery, TVars>(query: string, vars?: TVars, params?: OperationParameters): Promise<TQuery> {
         let res = await backoff(async () => {
-            try {
-                let r = await this.inner.query<TQuery, TVars>(query, vars, params);
-                return { type: 'result', result: r };
-            } catch (e) {
-                let eWrapper = this.errorHandler(e);
-                if (eWrapper) {
-                    console.warn(eWrapper);
-                    return { type: 'error', error: eWrapper };
-                } else {
-                    console.warn(e);
-                    throw e;
+            while (true) {
+                try {
+                    let r = await this.inner.query<TQuery, TVars>(query, vars, params);
+                    return { type: 'result', result: r };
+                } catch (e) {
+                    let processed = this.errorHandler(e);
+                    if (processed.type === 'error') {
+                        return { type: 'error', error: processed.error };
+                    } else if (processed.type === 'unknown') {
+                        console.warn(e);
+                        throw e;
+                    } else if (processed.type === 'retry') {
+                        await delay(Math.max(this.minRetry, processed.delay));
+                    }
                 }
             }
         });
@@ -52,17 +62,20 @@ export class RetryEngine implements GraphqlEngine {
     }
     async mutate<TMutation, TVars>(mutation: string, vars?: TVars): Promise<TMutation> {
         let res = await backoff(async () => {
-            try {
-                let r = await this.inner.mutate<TMutation, TVars>(mutation, vars);
-                return { type: 'result', result: r };
-            } catch (e) {
-                let eWrapper = this.errorHandler(e);
-                if (eWrapper) {
-                    console.warn(eWrapper);
-                    return { type: 'error', error: eWrapper };
-                } else {
-                    console.warn(e);
-                    throw e;
+            while (true) {
+                try {
+                    let r = await this.inner.mutate<TMutation, TVars>(mutation, vars);
+                    return { type: 'result', result: r };
+                } catch (e) {
+                    let processed = this.errorHandler(e);
+                    if (processed.type === 'error') {
+                        return { type: 'error', error: processed.error };
+                    } else if (processed.type === 'unknown') {
+                        console.warn(e);
+                        throw e;
+                    } else if (processed.type === 'retry') {
+                        await delay(Math.max(this.minRetry, processed.delay));
+                    }
                 }
             }
         });
@@ -78,6 +91,7 @@ export class RetryEngine implements GraphqlEngine {
         let currentWatch: GraphqlQueryWatch<TQuery> | undefined;
         let currentWatchSubscriber: (() => void) | undefined;
         let currentResult: GraphqlQueryResult<TQuery> | undefined;
+        let currentFailureCount = 0;
         let callbacks = new Map<string, (args: { data?: TQuery, error?: Error }) => void>();
         let resolved = false;
         let resolve!: () => void;
@@ -87,10 +101,13 @@ export class RetryEngine implements GraphqlEngine {
             reject = rj;
         });
 
-        let restart = () => {
+        let restart = (restartDelay: number) => {
             if (destroyed) {
                 return;
             }
+
+            // We need to destroy watch subscriptions and watch itself 
+            // immediatelly to avoid double invokes of restart function
             if (currentWatchSubscriber) {
                 currentWatchSubscriber();
                 currentWatchSubscriber = undefined;
@@ -99,64 +116,80 @@ export class RetryEngine implements GraphqlEngine {
                 currentWatch.destroy();
                 currentWatch = undefined;
             }
-            currentWatch = this.inner.queryWatch(query, vars, params);
-            let cr = currentWatch.currentResult();
-            if (cr) {
-                if (cr.error) {
-                    throw Error('Initial result can\'t be error!');
-                }
 
-                // Save and notify first value
-                currentResult = cr;
+            setTimeout(() => {
+                // Start new queryu watch
+                currentWatch = this.inner.queryWatch(query, vars, params);
 
-                // Resolve start promise if needed
-                if (!resolved) {
-                    resolved = true;
-                    if (currentResult.error) {
-                        reject();
-                    } else {
-                        resolve();
+                // Fetch cached value
+                let cr = currentWatch.currentResult();
+                if (cr) {
+
+                    // Errors are impossible since we had zero event loop ticks since starting watch
+                    // and error MUST be received from network.
+                    // At most we could have initial value from cache
+                    if (cr.error) {
+                        throw Error('Initial result can\'t be error!');
                     }
-                }
 
-                for (let i of callbacks.values()) {
-                    i(currentResult);
-                }
-            }
-            currentWatchSubscriber = currentWatch.subscribe((u) => {
-                if (destroyed) {
-                    return;
-                }
-                if (u.error) {
-                    let werror = this.errorHandler(u.error);
-                    if (werror) {
-                        console.warn(werror);
-                        currentResult = { error: werror };
-                        if (!resolved) {
-                            resolved = true;
-                            reject();
-                        }
-                        for (let i of callbacks.values()) {
-                            i(currentResult);
-                        }
-                    } else {
-                        console.warn(u.error);
-                        // Unknown error - restart
-                        restart();
-                    }
-                } else {
-                    currentResult = u;
+                    // Save and notify first value
+                    currentResult = cr;
+
+                    // Resolve start promise if needed
                     if (!resolved) {
                         resolved = true;
+                        currentFailureCount = 0;
                         resolve();
                     }
+
                     for (let i of callbacks.values()) {
                         i(currentResult);
                     }
                 }
-            });
+
+                // Start watch subscriber
+                currentWatchSubscriber = currentWatch.subscribe((u) => {
+                    if (destroyed) {
+                        return;
+                    }
+                    if (u.error) {
+                        let processed = this.errorHandler(u.error);
+
+                        if (processed.type === 'error') {
+                            currentFailureCount = 0;
+                            currentResult = { error: processed.error };
+                            if (!resolved) {
+                                resolved = true;
+                                reject();
+                            }
+                            for (let i of callbacks.values()) {
+                                i(currentResult);
+                            }
+                        } else if (processed.type === 'unknown') {
+                            if (currentFailureCount < 50) {
+                                currentFailureCount++;
+                            }
+                            let retryDelay = exponentialBackoffDelay(currentFailureCount, 1000, 5000, 50);
+                            restart(Math.max(this.minRetry, retryDelay));
+                        } else if (processed.type === 'retry') {
+                            currentFailureCount = 0;
+                            restart(Math.max(this.minRetry, processed.delay));
+                        }
+                    } else {
+                        currentFailureCount = 0;
+                        currentResult = u;
+                        if (!resolved) {
+                            resolved = true;
+                            resolve();
+                        }
+                        for (let i of callbacks.values()) {
+                            i(currentResult);
+                        }
+                    }
+                });
+            }, restartDelay);
         };
-        restart();
+        restart(10);
 
         return {
             subscribe: (handler: ((args: { data?: TQuery, error?: Error }) => void)) => {
